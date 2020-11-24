1069ff6b09dd4bdc287b81ac94f8cdd7c2c73424
natalie <njurcik4503@stu.d214.org>
2020-11-24 15:09:04 -0600

    finished

--- FunctionsLecture/ViewController.swift
+++ FunctionsLecture/ViewController.swift
@@ -28,20 +28,56 @@ class ViewController: UIViewController {
 
     @IBAction func voidFunctionPressed(_ sender: Any)
     {
-        
+        //Function Call
+        voidFunction()
     }
+    
     @IBAction func parameterFunctionPressed(_ sender: Any)
     {
+        let nameEntered = parameterFunctionTextfield.text!
         
+        //Function Call
+        parameterFunction(EnterNameHere: nameEntered)
     }
     @IBAction func returnStatementPressed(_ sender: Any)
     {
+        //Function Call
+        let valueReturned = returnFunction()
         
+        returnFunctionLabel.text = valueReturned
     }
     
     @IBAction func returnAndParametersFunction(_ sender: Any)
     {
+        let n1 = Int(textField1.text!)!
+        let n2 = Int(textField2.text!)!
         
+        let sum = add(number1: n1, number2: n2)
+        
+        functionWithBothLabel.text = "\(n1) + \(n2) = \(sum)"
+    }
+    
+    //Function Implementation for Void Function
+    func voidFunction() {
+        voidFunctionLabel.text = "Hello"
+    }
+    
+    //Function Implementation for Parameter Function
+    func parameterFunction(EnterNameHere name: String) {
+        parameterFunctionLabel.text = "Hello, \(name)"
+    }
+    
+    //Function Implementation for return statement
+    func returnFunction() -> String {
+        let name = "George"
+        return name
+    }
+    
+    //Function Implementation
+    func add(number1: Int, number2: Int) -> Int {
+        let answer = number1 + number2
+        
+        return answer
     }
     
 }
